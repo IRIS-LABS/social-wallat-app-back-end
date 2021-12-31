8f26ad29f5d562e06eaa3f44752a7a3802085479
DilshanSenarath <dilshan99news@gmail.com>
2021-12-31 19:19:28 +0530

    complete authentication

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,5 +4,6 @@ const handleRequest = require("../utils/requestHandler");
 
 router.post("/sign-up", handleRequest("auth", "signUp"));
 router.post("/sign-in", handleRequest("auth", "signIn"));
+router.get("/sign-out", handleRequest("auth", "logout"));
 
 module.exports = router;
